a74cb93f04e09a43412bdf3acba0af5fe1af9f12
andreresende36 <andreresende36@gmail.com>
2022-09-21 18:22:08 -0300

    Alterando formatação exercício 3

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script3.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script3.js
@@ -1,7 +1,7 @@
 const base = 5;
 const heigth = 8;
 const area = base * heigth;
-const perimeter = 2 * (base+heigth);
+const perimeter = 2 * (base + heigth);
 
 console.log(area);
 console.log(perimeter);
