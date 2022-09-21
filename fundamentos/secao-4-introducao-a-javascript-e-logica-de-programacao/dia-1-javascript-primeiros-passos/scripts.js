2a3a8cbd3124f833a8c4cc792d077e69e2aa6020
andreresende36 <andreresende36@gmail.com>
2022-09-21 15:47:27 -0300

    Resolução do primeiro exercício

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -0,0 +1,9 @@
+const myName = "André LUiz Dias Resende";
+const birthCity = "Belém";
+let birthYear = "1990";
+birthYear = "2030";
+// birthCity = "Goiânia";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
