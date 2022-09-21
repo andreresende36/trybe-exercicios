7e4746911a050df4f8592cce4e18d0c2b83358b2
andreresende36 <andreresende36@gmail.com>
2022-09-21 18:53:37 -0300

    Exercício operadores aritméticos

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operadores-aritmeticos.js
@@ -0,0 +1,19 @@
+let a = 5, b = 10;
+
+function soma(num1, num2) {
+  return num1 + num2;
+}
+function subtracao(num1, num2) {
+  return num1 - num2;
+}
+function multiplicacao(num1, num2) {
+  return num1 * num2;
+}
+function divisao(num1, num2) {
+  return num1 / num2;
+}
+
+console.log(soma(a,b));
+console.log(subtracao(a,b));
+console.log(multiplicacao(a,b));
+console.log(divisao(a,b));
