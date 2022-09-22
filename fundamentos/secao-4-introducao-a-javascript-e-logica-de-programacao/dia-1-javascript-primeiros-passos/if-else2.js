9ccda6e1c9069ad75e5f25a336abadd222f3aa98
andreresende36 <andreresende36@gmail.com>
2022-09-22 13:55:03 -0300

    Atualizacao exercício lucro 1000 unidades

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/if-else2.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/if-else2.js
@@ -1,24 +1,24 @@
-// // // Verifica se existe pelo menos um número par
-// const a = 3; b = 10; c = 5;
+// // Verifica se existe pelo menos um número par
+const a = 3; b = 10; c = 5;
 
-// if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
-//   console.log(true);
-// }
-// else {
-//   console.log(false);
-// }
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
 
-// // Verifica se existe pelo menos um número ímpar
-// const d = 3; e = 10; f = 18;
+// Verifica se existe pelo menos um número ímpar
+const d = 3; e = 10; f = 18;
 
-// if (d % 2 === 1 || e % 2 === 1 || f % 2 === 1) {
-//   console.log(true);
-// }
-// else {
-//   console.log(false);
-// }
+if (d % 2 === 1 || e % 2 === 1 || f % 2 === 1) {
+  console.log(true);
+}
+else {
+  console.log(false);
+}
 
-// Exercício custo e valor de venda
+Exercício custo e valor de venda
 
 const valorCusto = 50;
 const valorVenda = 500;
