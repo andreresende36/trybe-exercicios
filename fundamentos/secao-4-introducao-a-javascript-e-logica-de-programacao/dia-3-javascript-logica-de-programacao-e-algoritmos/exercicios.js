0b63961aa4ffddaf5325d479db34a2e620306c4d
andreresende36 <andreresende36@gmail.com>
2022-09-23 19:30:36 -0300

    Maior palavra

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -0,0 +1,40 @@
+// Exercício 1
+// O fatorial é representado pelo sinal !
+// 4! = 4 x 3 x 2 x 1 = 24
+
+let fatorial = 1;
+let num = 10;
+
+for (index = num; index > 0; index -= 1){
+  fatorial = fatorial * index;
+}
+
+console.log('O fatorial de ' + num + ' é: ' + fatorial);
+
+// Inverter palavra
+
+let word = 'tryber';
+let inverso = [];
+
+for (let letra of word){
+  inverso.unshift(letra);
+}
+console.log(inverso.join(''));
+
+// Contagem em array
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let contador = 0;
+let maiorContagem = 0;
+let maiorPalavra = '';
+
+for (index = 0; index < array.length; index += 1){
+  for (let letra of array[index]){
+    contador += 1;
+  }
+  if (contador > maiorContagem){
+    maiorContagem = contador;
+    maiorPalavra = array[index];
+  }
+  contador = 0;
+}
+console.log(maiorPalavra);
