ad8196554c41d621043382f67f5bc81f882ebc64
andreresende36 <andreresende36@gmail.com>
2022-09-26 19:30:38 -0300

    Resolução da primeira página de exercícios 1

---
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,31 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+info.recorrente = 'Sim';
+
+// console.log('Bem-vinda, ' + info.personagem);
+// console.log(info);
+// for (let key in info){
+//   console.log(key);
+// }
+// for (let key in info){
+//   console.log(info[key]);
+// }
+
+let info2 ={
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+for (let key in info){
+  if (info[key] === info2[key]){
+    console.log('Ambos recorrentes');
+  }
+  else{
+    console.log(info[key] + ' e ' + info2[key]);
+  }  
+}
