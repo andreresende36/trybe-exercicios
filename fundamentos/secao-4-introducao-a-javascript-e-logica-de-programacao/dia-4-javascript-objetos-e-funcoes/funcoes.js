05565322af8bbe6d2f57208ca7e729d4026e12eb
andreresende36 <andreresende36@gmail.com>
2022-09-26 22:39:05 -0300

    Resolução Maior Palavra

--- fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
+++ fundamentos/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funcoes.js
@@ -31,17 +31,38 @@
 // let numeros = [2, 3, 6, 7, 10, 1];
 // console.log(retornaMaiorIndex(numeros));
 
-// Retorna maior índice
-function retornaMenorIndex(array) {
-  let menorValor = Number.POSITIVE_INFINITY;
-  let menorIndice = Number.NEGATIVE_INFINITY;
-  for (index = 0; index < array.length; index += 1) {
-    if (array[index] < menorValor){
-      menorValor = array[index];
-      menorIndice = index;
+// Retorna menor índice
+// function retornaMenorIndex(array) {
+//   let menorValor = Number.POSITIVE_INFINITY;
+//   let menorIndice = Number.NEGATIVE_INFINITY;
+//   for (index = 0; index < array.length; index += 1) {
+//     if (array[index] < menorValor){
+//       menorValor = array[index];
+//       menorIndice = index;
+//     }
+//   }
+//   return menorIndice;
+// }
+// let numeros = [2, 4, 6, 7, 10, 0, -3];
+// console.log(retornaMenorIndex(numeros));
+
+// Maior palavra do array
+function retornaMaiorPalavra (array){
+  let contador = 0;
+  let numLetrasMaiorPalavra = 0;
+  let maiorPalavra;
+  for (index = 0; index < array.length; index += 1){
+    for (let letra of array[index]){
+      contador += 1;
     }
+    if (contador > numLetrasMaiorPalavra){
+      numLetrasMaiorPalavra = contador;
+      maiorPalavra = array[index];
+    }
+    contador = 0;
   }
-  return menorIndice;
+  return maiorPalavra;
 }
-let numeros = [2, 4, 6, 7, 10, 0, -3];
-console.log(retornaMenorIndex(numeros));
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+console.log(retornaMaiorPalavra(nomes));
