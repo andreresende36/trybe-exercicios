970ba6513dafce41884ad24313d756524078442a
andreresende36 <andreresende36@gmail.com>
2022-09-29 18:46:01 -0300

    Treino de criação de elementos

--- fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,7 +1,28 @@
-document.getElementById('elementoOndeVoceEsta').parentNode.style.backgroundColor = 'green';
-document.getElementById('primeiroFilhoDoFilho').innerText = 'Esse é o primeiro filho.'
-console.log(document.getElementById('pai').firstElementChild);
-console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
-console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
-console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
-console.log(document.getElementById('pai').children[2]);
+// document.getElementById('elementoOndeVoceEsta').parentNode.style.backgroundColor = 'green';
+// document.getElementById('primeiroFilhoDoFilho').innerText = 'Esse é o primeiro filho.'
+// console.log(document.getElementById('pai').firstElementChild);
+// console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+// console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+// console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+// console.log(document.getElementById('pai').children[2]);
+
+let pai = document.querySelector('#pai');
+let elementoOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let irmaoElementoOndeVoceEsta = document.createElement('section');
+let filhoElementoOndeVoceEsta = document.createElement('section');
+let filhoprimeiroFilhoDoFilho = documento.createElement('section');
+
+irmaoElementoOndeVoceEsta.id = 'ultimoFilhoMesmo';
+filhoElementoOndeVoceEsta.id = 'filhoDeOndeEstou';
+filhoprimeiroFilhoDoFilho.id = 'filhoDoprimeiroFilhoDoFilho';
+
+pai.appendChild(irmaoElementoOndeVoceEsta);
+elementoOndeVoceEsta.appendChild(filhoElementoOndeVoceEsta);
+primeiroFilhoDoFilho.appendChild(filhoprimeiroFilhoDoFilho);
+
+console.log(pai);
+console.log(irmaoElementoOndeVoceEsta);
+console.log(pai.children);
+console.log(elementoOndeVoceEsta.children);
+console.log(primeiroFilhoDoFilho.children);
