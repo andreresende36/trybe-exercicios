f1792d9c9621ceff89718e9460c791164314b0fc
andreresende36 <andreresende36@gmail.com>
2022-09-30 19:30:40 -0300

    Resolução da Parte 1

--- fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/secao-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -11,6 +11,28 @@ function createDaysOfTheWeek() {
   };
 }
 
-createDaysOfTheWeek();
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let days = document.getElementById('days');
 
+function createDaysOfTheMonth(){
+  for (i = 0; i < decemberDaysList.length; i++){
+    let itemLista = document.createElement('li');
+    itemLista.innerText = decemberDaysList[i];
+    itemLista.className = 'day';
+    days.appendChild(itemLista);
+
+    if (decemberDaysList[i] === 4 || decemberDaysList[i] === 11 || decemberDaysList[i] === 18 || decemberDaysList[i] === 25){
+      itemLista.className += ' friday';
+    }
+
+    if (decemberDaysList[i] === 24 || decemberDaysList[i] === 25 || decemberDaysList[i] === 31){
+      itemLista.className += ' holiday';
+    }
+  }
+}
+
+createDaysOfTheWeek();
+createDaysOfTheMonth();
+
+console.log(days.children);
 // Escreva seu código abaixo.
