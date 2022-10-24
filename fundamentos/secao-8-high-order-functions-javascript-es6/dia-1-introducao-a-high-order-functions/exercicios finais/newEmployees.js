68da379f50524e1898a3cf0b3d21a6468c51b5ef
andreresende36 <andreresende36@gmail.com>
2022-10-24 18:48:00 -0300

    Resolução do Exercício F inal 1

---
+++ fundamentos/secao-8-high-order-functions-javascript-es6/dia-1-introducao-a-high-order-functions/exercicios finais/newEmployees.js
@@ -0,0 +1,24 @@
+const employeeReg = (name) =>{
+  const arrayName = name.split(' ');
+  let email = '';
+  arrayName.forEach((element,i) => {
+    if(i+1 < arrayName.length){
+      email += `${element.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()}_`
+    } else {
+      email += `${element.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()}@trybe.com`
+    }
+  });
+  
+  return {nomeCompleto: name, email: email}
+}
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: func('Luiz Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees(employeeReg));
