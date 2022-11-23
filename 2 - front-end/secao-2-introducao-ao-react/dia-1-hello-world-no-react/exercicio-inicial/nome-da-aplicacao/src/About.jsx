2b0f1f4b28f2e7e8e7fe2c14d0ead7e78ed8e6ad
andreresende36 <andreresende36@gmail.com>
2022-11-23 15:08:36 -0300

    Finalização do dia 1

---
+++ 2 - front-end/secao-2-introducao-ao-react/dia-1-hello-world-no-react/exercicio-inicial/nome-da-aplicacao/src/About.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class About extends React.Component {
+  render () {
+    return (
+      <>
+        <h1>André Luiz Dias Resende</h1>
+        <p>Sou top demais!</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Javascript</li>
+          <li>React</li>
+          <li>Next.js</li>
+        </ul>
+      </>
+    )
+  }
+}
+
+export default About
