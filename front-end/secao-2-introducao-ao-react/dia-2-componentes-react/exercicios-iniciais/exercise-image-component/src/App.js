5d14614eb971dd183ebe6d0457762a8f1f6aaa95
andreresende36 <andreresende36@gmail.com>
2022-11-23 15:35:13 -0300

    Exercicio Inicial 1

---
+++ front-end/secao-2-introducao-ao-react/dia-2-componentes-react/exercicios-iniciais/exercise-image-component/src/App.js
@@ -0,0 +1,12 @@
+import Image from './Image';
+
+import './App.css';
+
+function App() {
+  const url = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';
+  return (
+    <Image source={url} alternativeText={"Cute cat staring"} />
+  );
+}
+
+export default App;
