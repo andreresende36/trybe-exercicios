d80f465defc7e941b4586907a52479a954b8f173
andreresende36 <andreresende36@gmail.com>
2022-11-24 11:18:35 -0300

    Finalização do dia

---
+++ front-end/secao-2-introducao-ao-react/dia-2-componentes-react/exercicios-iniciais/exercise-listing-components/src/UserProfile.js
@@ -0,0 +1,19 @@
+// arquivo UserProfile.js
+import React from 'react';
+import Image from './Image';
+import PropTypes from 'prop-types';
+
+class UserProfile extends React.Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        <p> { user.name } </p>
+        <p> { user.email } </p>
+        <Image source={ user.avatar } alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
