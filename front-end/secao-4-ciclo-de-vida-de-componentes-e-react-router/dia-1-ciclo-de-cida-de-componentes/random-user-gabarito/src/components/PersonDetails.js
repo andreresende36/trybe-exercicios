1ed39d721af2e45c6d61f355d5a90410caece2a8
andreresende36 <andreresende36@gmail.com>
2022-12-08 18:49:26 -0300

    Exercicios de fixação

---
+++ front-end/secao-4-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-cida-de-componentes/random-user-gabarito/src/components/PersonDetails.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Loading from './Loading';
+import PersonCard from './PersonCard';
+
+class PersonDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: true,
+      person: [],
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://api.randomuser.me/';
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          person: data.results,
+          loading: false,
+        });
+      });
+  }
+
+  getUserElements(user) {
+    return {
+      name: `${user.name.first} ${user.name.last}`,
+      email: user.email,
+      age: user.dob.age,
+      image: user.picture.thumbnail,
+    };
+  }
+
+  render() {
+    const { loading, person } = this.state;
+    if (loading) return <Loading />;
+    return (
+      <div>
+        <PersonCard
+          person={ this.getUserElements(person[0]) }
+        />
+      </div>
+    );
+  }
+}
+
+export default PersonDetails;
